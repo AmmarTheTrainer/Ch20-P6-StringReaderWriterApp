b85edfe806965fcb7d85394d6e039a08c2a13daa
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-13 12:53:29 +0500

    final update

--- Ch20-P6-StringReaderWriterApp/Program.cs
+++ Ch20-P6-StringReaderWriterApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace Ch20_P6_StringReaderWriterApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with StringWriter / StringReader *****\n");
+            // Create a StringWriter and emit character data to memory.
+            //using (StringWriter strWriter = new StringWriter())
+            //{
+            //    strWriter.WriteLine("Don't forget Mother's Day this year...");
+            //    // Get a copy of the contents (stored in a string) and dump
+            //    // to console.
+            //    Console.WriteLine("Contents of StringWriter:\n{0}", strWriter);
+            //}
+
+            //using (StringWriter strWriter = new StringWriter())
+            //{
+            //    strWriter.WriteLine("Don't forget Mother's Day this year...");
+            //    Console.WriteLine("Contents of StringWriter:\n{0}", strWriter);
+            //    // Get the internal StringBuilder.
+            //    StringBuilder sb = strWriter.GetStringBuilder();
+            //    sb.Insert(0, "Hey!! ");
+            //    Console.WriteLine("-> {0}", sb.ToString());
+            //    sb.Remove(0, "Hey!! ".Length);
+            //    Console.WriteLine("-> {0}", sb.ToString());
+            //}
+            using (StringWriter strWriter = new StringWriter())
+            {
+                 strWriter.WriteLine("Don't forget Mother's Day this year...");
+                 Console.WriteLine("Contents of StringWriter:\n{0}", strWriter);
+                 // Read data from the StringWriter.
+                 using (StringReader strReader = new StringReader(strWriter.ToString()))
+                 {
+                     string input = null;
+                     while ((input = strReader.ReadLine()) != null)
+                     {
+                         Console.WriteLine(input);
+                     }
+
+                     Console.ReadLine();
+                 }
+            }
         }
     }
 }
